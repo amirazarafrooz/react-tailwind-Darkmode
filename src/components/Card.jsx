1b4ec46774f8aa5d1402bf4930f148549bf1168d
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-12-01 21:09:54 +0330

    feat: add landing card

---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import light from "../assets/images/present-light.svg";
+import dark from "../assets/images/present-dark.svg";
+
+const Card = ({theme}) => {
+
+
+    return (
+        <div className="flex justify-center items-center rounded-2xl shadow-2xl dark:bg-[#2f3e46] bg-[#90e0ef] w-[1200px] h-[500px] px-6 transition-all">
+
+        {/* text  */}
+        <div className="w-1/2 flex justify-center items-start flex-col  px-4">
+          <p className="text-4xl font-bold dark:text-white  ">hey</p>
+          <p className="text-4xl font-bold dark:text-white mt-4">I'm Amirhosein </p>
+          <p className="text-lg font-medium dark:text-gray-300  mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            magnam, harum quasi tenetur ipsum doloremque repellat sint voluptate
+            neque voluptatibus consectetur, iste eos dolorem! Sunt rerum
+            reprehenderit adipisci maiores soluta atque necessitatibus maxime,
+            recusandae provident voluptatem culpa ipsa itaque iste!
+          </p>
+          <button className="mt-10 rounded-2xl px-5 py-2 dark:bg-[#81b29a] bg-[#0077b6] text-xl font-bold text-white  flex justify-center items-center">contact me!</button>
+        </div>
+        {/* image */}
+        <div className="w-1/2 flex justify-center items-center">
+          {theme === "dark" ? <img src={dark} className="h-[500px] w-[500px]"></img>
+          : (
+            <img src={light} className="h-[1000px] w-[1000px]"></img>
+          )}
+        </div>
+  
+        </div>
+  
+    );
+};
+
+export default Card;
